4b6466b73f888b95eba8d2e2888a1d167e41ca84
Boz <mondohamoeva@hotmail.fr>
2024-12-18 09:07:45 +0100

    {exit button working}

--- code/Faces/face12.gd
+++ code/Faces/face12.gd
@@ -1,14 +1,29 @@
 extends Face
 
-
+var canquit: bool = false
+@export var canexit: Timer 
 func _ready() -> void:
 	pass 
 
 func _face_on():
+	canexit.start()
 	pass
 
 func _face_off():
-	pass
+	canquit = false
+	canexit.stop()  # Stoppe le timer si la face est désactivée
+
 
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_area_3d_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
+	if event.is_action_pressed("ui_accept") and canquit:
+		get_tree().quit()
+	pass # Replace with function body.
+
+
+func _on_can_exit_timeout() -> void:
+	canquit = true
+	pass # Replace with function body.

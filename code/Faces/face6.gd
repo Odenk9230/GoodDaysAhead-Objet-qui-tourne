333b75c0b78c88e453d81a1dc999e3ee648481c9
Sammouille <77251950+Sammouille@users.noreply.github.com>
2024-12-19 16:04:55 +0100

    Musique integrée pouyr la roue

--- code/Faces/face6.gd
+++ code/Faces/face6.gd
@@ -7,10 +7,12 @@ var relache = false
 var appui = false
 var dose_appui : float = 0.0
 
+@onready var musique = $MeshRoue/Musique/Musique
+
 var active = false
 
 func _ready() -> void:
-	pass
+	musique.stream_paused = true
 
 func _face_on():
 	active = true
@@ -49,7 +51,10 @@ func _on_pointeur_area_entered(area: Area3D) -> void:
 				1:
 					roue._faire_tourner(-randf_range(1,15))
 				2:
-					print("MUSIQUE")
+					if musique.stream_paused :
+						musique.stream_paused = false
+					else :
+						musique.stream_paused = true
 				3:
 					print("RETOUR")
 				4:

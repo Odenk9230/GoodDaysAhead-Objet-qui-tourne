5f4d8afd8a663580020de04de8a33e227b37a157
Boz <mondohamoeva@hotmail.fr>
2024-12-19 17:14:01 +0100

    { fixing the button

--- code/Faces/face4.gd
+++ code/Faces/face4.gd
@@ -1,18 +1,46 @@
 extends Face
 
-
-func _ready() -> void:
-	pass 
+@export var led: MeshInstance3D
+@export var begin: Color
+@export var win: Color
+@export var loose: Color
+@export var anim : AnimationPlayer
+@export var Sprite : AnimatedSprite3D
+@export var label: Label3D
+@export var punchtimer: Timer
+var canpunch = false
+var conteur = 0
 
 func _face_on():
+	led.mesh.surface_get_material(0).set_emission(begin)
+	punchtimer.start()
+	canpunch = true
 	pass
 
 func _face_off():
 	pass
 
 func _process(delta: float) -> void:
+	label.text = str(conteur)
 	pass
 
 
 func _on_area_3d_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
+	if Input.is_action_just_pressed("clic_gauche") and canpunch:
+		anim.play("CLICK")
+		Sprite.play("default")
+		conteur = conteur + 1
+		label.text = str(conteur)
+	pass # Replace with function body.
+
+
+func _on_punchtimer_timeout() -> void:
+	if conteur >= 30 :
+		led.mesh.surface_get_material(0).set_emission(win)
+		canpunch = false
+		conteur = 30
+	else:
+		led.mesh.surface_get_material(0).set_emission(loose)
+		conteur = 0
+		punchtimer.start()
 	pass # Replace with function body.
--- scene/nuclear_button_script.gd
+++ scene/nuclear_button_script.gd
@@ -2,20 +2,27 @@ extends Node3D
 @export var anim : AnimationPlayer
 @export var Sprite : AnimatedSprite3D
 var conteur = 0
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@export var led: MeshInstance3D
+# # Called when the node enters the scene tree for the first time.
+# func _ready() -> void:
+# 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+# # Called every frame. 'delta' is the elapsed time since the previous frame.
+# func _process(delta: float) -> void:
+# 	pass
 
 
-func _on_area_3d_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
-	if Input.is_action_just_pressed("clic_gauche"):
-		print("Click")
-		anim.play("CLICK")
-		Sprite.play("default")
-		conteur = conteur + 1
-		$"../../retro_screen2/Label3D".text = str(conteur)
+# func _on_area_3d_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
+# 	if Input.is_action_just_pressed("clic_gauche"):
+# 		print("Click")
+# 		anim.play("CLICK")
+# 		Sprite.play("default")
+# 		conteur = conteur + 1
+# 		$"../../retro_screen2/Label3D".text = str(conteur)
+
+
+# func _on_punchtimer_timeout() -> void:
+# 	if conteur > 50 :
+# 		led
+# 	pass # Replace with function body.

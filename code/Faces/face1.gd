7b354c3d06974ae43960bf20b897e345a9bbb1b4
Sammouille <77251950+Sammouille@users.noreply.github.com>
2024-12-20 17:34:47 +0100

    last push

--- code/Faces/face1.gd
+++ code/Faces/face1.gd
@@ -1,5 +1,12 @@
 extends Face
 
+var clicked = false
+
+@export var sensibilite = 1.0
+
+@onready var potard = $Area3D/MeshInstance3D
+
+var accelerotation : Vector2 = Vector2(0,0)
 
 func _ready() -> void:
 	pass 
@@ -11,4 +18,21 @@ func _face_off():
 	pass
 
 func _process(delta: float) -> void:
-	pass
+	var mod_db = (accelerotation.x - accelerotation.y)*delta
+	potard.rotate_y(accelerotation.x * delta)
+	potard.rotate_y(-accelerotation.y * delta)
+	AudioServer.set_bus_volume_db(0, AudioServer.get_bus_volume_db(0)+mod_db)
+	accelerotation *= 0.3
+
+
+func _on_area_3d_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
+	if Input.is_action_just_pressed("clic_droit") and !clicked:
+		clicked = true
+
+func _unhandled_input(event: InputEvent) -> void: 
+	if Input.is_action_just_released("clic_droit") and clicked:
+		clicked = false
+	
+	if event is InputEventMouseMotion:
+		if clicked :
+			accelerotation += event.relative * sensibilite
